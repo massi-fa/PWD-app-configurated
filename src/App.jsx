c059ae6b4990c76b6d6dd3cf0832f0684f4aa8f1
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2021-10-05 20:17:31 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components/macro';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ciao</h1>
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Roboto', sans-serif;
+    transition: all 0.25s;
+  }
+  body {
+    overflow: hidden;
+    height: 100%;
+  } 
+`;
+
+const Container = styled.div`
+  height: 100%;
+`;
+
+const Text = styled.h1`
+
+`;
+
+const App = () => (
+  <Container>
+    <GlobalStyle />
+    <Text>Ciao</Text>
+  </Container>
+);
 
 export default App;
