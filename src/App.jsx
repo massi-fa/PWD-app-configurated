face46b905fd7afcf8e223b31181ccbf291538f9
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2021-10-18 00:29:56 +0200

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components/macro';
+import Header from './components/Header';
 
 const GlobalStyle = createGlobalStyle`
   * {
     font-family: 'Roboto', sans-serif;
     transition: all 0.25s;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
   body {
-    overflow: hidden;
     height: 100%;
   } 
 `;
@@ -16,14 +19,10 @@ const Container = styled.div`
   height: 100%;
 `;
 
-const Text = styled.h1`
-
-`;
-
 const App = () => (
   <Container>
     <GlobalStyle />
-    <Text>Ciao</Text>
+    <Header />
   </Container>
 );
 
